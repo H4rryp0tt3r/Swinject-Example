a0e3183e7264dbc2dac79c892fc5d605e0f13ea2
H4rryp0tt3r <nagesh.podilapu@gmail.com>
2017-01-29 19:26:36 +0530

    Create a new iOS single page application in Xcode

---
+++ SwinjectExample/SwinjectExampleTests/SwinjectExampleTests.swift
@@ -0,0 +1,14 @@
+import XCTest
+@testable import SwinjectExample
+
+class SwinjectExampleTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    override func tearDown() {
+        super.tearDown()
+    }
+
+}
